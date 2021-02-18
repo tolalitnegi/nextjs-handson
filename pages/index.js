2093df68f8762f093e01c1976c1b991441dfb7dd
Lalit Negi <tolalitnegi@gmail.com>
2021-02-18 16:52:35 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+  <div>
+    <h1>Welcome to nextjs, this is from pages/index.js</h1>
+  </div>
+);
+
+export default Index;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Nextjs handson
+
+
+# setup
+- nvm use 14
+- npm i
+- npm start
