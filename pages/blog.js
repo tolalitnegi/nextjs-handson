5a7875d8562f9c447ad180833265100784df8a6a
Lalit Negi <tolalitnegi@gmail.com>
2021-03-14 12:16:22 +0000

    using withRouter instead of url

--- pages/blog.js
+++ pages/blog.js
@@ -2,20 +2,29 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
 
+/**
+ * To pass a query param or a path param slug value
+ * for path para two options
+ *  <Link href="/post?title=React"><a>React Post</a></Link>
+ * could be
+ *  <Link as="react-post" href="/post?title=React"><a>React Post</a></Link>
+ * final url will be /post/react-post
+ *  
+ */
 export default () => (
   <Layout title="My Blog">
     <ul>
       <li>
-        <Link href="/post?title=React"><a>React Post</a></Link>
+        <Link as="react-post" href="/post?title=ReactPost"><a>React Post</a></Link>
       </li>
       <li>
-        <Link href="/post?title=Angular"><a>Angular</a></Link>
+        <Link as="angular-post" href="/post?title=Angular"><a>Angular</a></Link>
       </li>
       <li>
-        <Link href="/post?title=TypeScript"><a>TypeScript</a></Link>
+        <Link as="typescript-post" href="/post?title=TypeScript"><a>TypeScript</a></Link>
       </li>
       <li>
-        <Link href="/post?title=GraphQl"><a>Graphql</a></Link>
+        <Link as="graphql-post" href="/post?title=GraphQl"><a>Graphql</a></Link>
       </li>
     </ul>
   </Layout>
--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,12 @@
 
 import Layout from '../components/Layout';
+import {withRouter} from 'next/router';
 
-const Post = ({ url }) => (
-  <Layout title={url.query.title}>
-    <p>lorem ipsum... {url.query.title}</p>
+// const Post = ({ url }) => ( // url is depricated
+const Post = ({ router }) => (
+  <Layout title={router.query.title}>
+    <p>lorem ipsum... {router.query.title}</p>
   </Layout>
 )
 
-export default Post;
+export default withRouter(Post);
